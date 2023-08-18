28f080f75a5fa358b4a28c0b93cda09c06b4c6ea
Guido <67983976+guidocesa@users.noreply.github.com>
2023-08-18 14:07:03 -0300

    Agregue comportamineot al Drill y otros caambios

--- drill_pod.cs
+++ drill_pod.cs
@@ -3,26 +3,54 @@ using System;
 
 public partial class drill_pod : RigidBody2D
 {
+	private Timer myTimer;
+	private bool active = true;
+	private float angleCorrection = MathF.PI/2;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.myTimer = this.GetNode<Timer>("Timer");
+		this.myTimer.Timeout += this.FinalExplosion;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Input.IsActionJustPressed("drill_direction_change") && this.active)
+		{
+			Vector2 newDirection = (GetGlobalMousePosition() - this.GlobalPosition).Normalized();
+			this.LinearVelocity = newDirection * this.LinearVelocity.Length();
+			this.Rotation = newDirection.Angle() + angleCorrection;
+			
+		}
 	}
 
 	public void OnBodyEntered(Node2D body)
 	{
-		GD.Print("Choco");
+		this.Sleeping = true;
+		this.active = false;
 
 		if(body.GetGroups().Contains("Minable"))
 		{
-			GD.Print("Con un minable");
-			this.Sleeping = true;
-			this.GetChild<CpuParticles2D>(0).Emitting = true;
+			this.GetNode<CpuParticles2D>("Mining").Emitting = true;
+			this.GetNode<PointLight2D>("PointLight2D").Enabled = true;
 		}
+		else
+		{
+			this.GetNode<CpuParticles2D>("Explosion").Emitting = true;
+			this.myTimer.Start(0.3);
+		}
+		
+		this.CollisionLayer = 0;
+		this.CollisionMask = 2;
+	}
 
+	private void FinalExplosion()
+	{
+		this.GetNode<Sprite2D>("Sprite2D").Visible = false;
+		this.GetNode<CpuParticles2D>("Explosion").Emitting = false;
+		this.myTimer.Timeout -= this.FinalExplosion;
+		this.myTimer.Timeout += this.QueueFree;
+		this.myTimer.Start(1);
 	}
 }
